123b4b92655d334a9181f65c245b3e5e76032889
Angus Redford <ar267@st-andrews.ac.uk>
2019-10-16 23:35:04 +0100

    fixed and named the columes of the results

--- lmBoot.r
+++ lmBoot.r
@@ -2,13 +2,15 @@ lmBoot <- function(inputData,y,ycol, nboot){
   
   col_names <- colnames(inputData)
   col_names <- col_names[-ycol]
+  bootResults<- matrix(NA,nrow =(length(col_names)+1),ncol = nboot)
+  colnames(bootResults) <- c("intercept",col_names)
     
   
   for(i in 1:nboot){
     
     # resample our data with replacement
     bootData <- inputData[sample(1:nrow(inputData), nrow(inputData), replace = T),]
-    bootResults<- matrix(NA,nrow =(length(col_names)+1),ncol = nboot)
+    
     
     # fit the model under this alternative reality
     bootLM <- lm(paste(paste(deparse(substitute(y))," ~",sep = ""),paste(col_names,collapse = " + "),collapse = " "), data = bootData)
@@ -26,3 +28,4 @@ lmBoot <- function(inputData,y,ycol, nboot){
 #testing2
 
 # https://www.math.ucla.edu/~anderson/rw1001/library/base/html/paste.html
+# https://www.dummies.com/programming/r/how-to-name-matrix-rows-and-columns-in-r/
